1dc7ca523e8a4058413e23bed7aa7e6248c23ec4
BardiaMadadi <bardiamadadi86@gmail.com>
2022-03-18 18:07:04 +0330

    Corona Class with functions  Added

---
+++ class/Corona.php
@@ -0,0 +1,100 @@
+<?php
+
+class Corona
+{
+
+    // private int $todayCases;
+    // private int $todayDeath;
+    // private int $todayRecovered;
+    // private int $allCases;
+    // private int $allDeath;
+    // private int $allRecovered;
+
+    public function getTodayCases()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["todayCases"];
+        }else{
+            return null;
+        }
+    }
+
+    public function getAllCases()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["cases"];
+        }else{
+            return null;
+        }
+    }
+    public function getAllDeath()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["deaths"];
+        }else{
+            return null;
+        }
+    }
+
+    public function getTodayDeath()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["todayDeaths"];
+        }else{
+            return null;
+        }
+    }
+    public function getTodayRecovered()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["todayRecovered"];
+        }else{
+            return null;
+        }
+    }
+
+    public function getAllRecovered()
+    {
+        include_once "../helper/curl.php";
+        $response =
+        json_decode(
+            makeAPICall("https://disease.sh/v3/covid-19/countries/Iran"),
+            true
+        );
+        if($response != null){
+            return $response["recovered"];
+        }else{
+            return null;
+        }
+    }
+}
